87ab9aa4bbdc5da4ff4285e48481785b2b8a28b2
DG <dgdivyanshu@gmail.com>
2019-03-26 13:19:38 +0530

    Partially completed LRU

--- pageReplacement.py
+++ pageReplacement.py
@@ -11,7 +11,22 @@ def fifo(page_list,frames_no):
 			frames.append(page)
 	hr=float(hit)/attempts
 	print "hits:%d Total attempts:%d Hit Ratio:%.2f "%(hit,attempts, hr)
+
+def lru(page_list, frames_no):
+	frames=[]
+	recent=[]
+	hit=0
+	attempts=len(page_list)
 	
+	for page in page_list:
+		if page in lru_q:
+			hit+=1
+		else:
+			if len(frames)<frames_no:
+				frames.append(page)
+			else:
+								
+
 page_list=[1,3,0,3,5,6,3]
 fifo(page_list,3)
 fifo(page_list,4)	
